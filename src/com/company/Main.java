54cfac6f760ecfde26b521eda51e6c116be8835d
Boldeusz <mnpower@poczta.onet.pl>
2021-01-21 12:35:31 +0100

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,25 +3,43 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int count = 1;
-        while (count != 6){
-            System.out.println("Count value is " +count);
-            count++;
-        }
-        count =1;
-        while (true){
-            if (count == 6){
-                break;
-            }
-            System.out.println("Count value is " + count);
-            count++;
-        }
-        count = 1;
-        do {
-            System.out.println("Count value is " + count);
-            count++;
-        }while (count != 6);
+//        int count = 1;
+//        while (count != 6){
+//            System.out.println("Count value is " +count);
+//            count++;
+//        }
+//        count =1;
+//        while (true){
+//            if (count == 6){
+//                break;
+//            }
+//            System.out.println("Count value is " + count);
+//            count++;
+//        }
+//        count = 1;
+//        do {
+//            System.out.println("Count value is " + count);
+//            count++;
+//        }while (count != 6);
+        int number = 4;
+        int finishNumber = 20;
 
+        while (number <= finishNumber){
+            number++;
+            if (!isEvenNumber(number)){
+                continue;
+            }
+            System.out.println("Even number " + number);
+        }
     }
+
+    public static boolean isEvenNumber(int number) {
+        if ((number % 2) == 0){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
 }
 
